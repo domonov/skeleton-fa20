d8207bf733d0117c2d3905836824e7b088c585eb
Davronbek Omonov <domonov@berkeley.edu>
2021-01-23 11:39:09 -0800

    ArrayDeque and LinkedListDeque created: iMac

---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,39 @@
+public class LinkedListDeque<Item> {
+
+
+
+
+
+    public void addFirst(Item x) {
+
+    }
+
+    public void addLast(Item x) {
+
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public void printDeque() {
+
+    }
+
+    public Item removeFirst() {
+        return null;
+    }
+
+    public Item removeLast() {
+        return null;
+    }
+
+    public Item getIndex(int i) {
+        return null;
+    }
+
+}
