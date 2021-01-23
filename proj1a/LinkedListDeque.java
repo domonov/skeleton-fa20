6c119f1b023da8eb4f6a7980ee9c9636f5abc3dc
Davronbek Omonov <domonov@berkeley.edu>
2021-01-23 13:17:05 -0800

    LinkedListDeque.get and getRecursive needs fixing: thinkpad

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,39 +1,98 @@
 public class LinkedListDeque<Item> {
+    private class Node {
+        Item item;
+        Node prev;
+        Node next;
 
+        public Node(Item x, Node p, Node n) {
+            item = x;
+            prev = p;
+            next = n;
+        }
+    }
 
+    public Node sentinel;
+    private int size;
 
-
+    public LinkedListDeque() {
+        sentinel = new Node(null, null, null);
+        size = 0;
+    }
 
     public void addFirst(Item x) {
-
+        if (isEmpty()) {
+            sentinel.next = new Node(x, sentinel, sentinel);
+        } else {
+            sentinel.next = new Node(x, sentinel, sentinel.next);
+        }
+        size += 1;
     }
 
     public void addLast(Item x) {
-
+        if (isEmpty()) {
+            sentinel.prev = new Node(x, sentinel, sentinel);
+        } else {
+            sentinel.prev = new Node(x, sentinel.prev, sentinel);
+        }
+        size += 1;
     }
 
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     public int size() {
-        return 0;
+        return size;
     }
 
     public void printDeque() {
-
+        Node p = sentinel;
+        while (p.next != null) {
+            System.out.print(p.item.toString() + " ");
+            p = p.next;
+        }
+        System.out.println();
     }
 
     public Item removeFirst() {
-        return null;
+        if (isEmpty()) {
+            return null;
+        }
+        Node target = sentinel.next;
+        sentinel.next = sentinel.next.next;
+        sentinel.next.prev = sentinel;
+        size -= 1;
+        return target.item;
     }
 
     public Item removeLast() {
+        if (isEmpty()) {
+            return null;
+        }
+        Node target = sentinel.prev;
+        sentinel.prev = sentinel.prev.prev;
+        sentinel.prev.next = sentinel;
+        size -= 1;
+        return target.item;
+    }
+
+
+    /** Returns the Item at the index, using iteration. */
+    public Item get(int index) {
+        if (index < size) {
+            Node p = sentinel;
+            int counter = 0;
+            while (p.next != null && counter < index) {
+                p = p.next;
+                counter += 1;
+            }
+            return p.item;
+        }
         return null;
     }
 
-    public Item getIndex(int i) {
+    /** Returns the Item at the index, using recursion. */
+    public Item getRecursive(int index) {
         return null;
     }
-
 }
